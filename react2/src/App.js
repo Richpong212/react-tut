7be41302352402860a0667f453ddf10a93fdf3a5
“Richpong212” <you@example.com>
2023-01-07 23:37:52 +0100

    lec

---
+++ react2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddNumber from './component/addNumber'
+
+const App = () => {
+  return (
+    <div>
+      <AddNumber />
+    </div>
+  )
+}
+
+export default App
---
+++ react2/src/component/addNumber.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddNumber = () => {
+
+
+  return (
+    <div>
+        <h1>addNumber</h1>
+        <span>200 time 4000690 is = {200 * 4000690} </span>
+    </div>
+  )
+}
+
+export default AddNumber
